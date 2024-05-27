8cec21a435eb8c529eb193a642a8a201cd9e6187
Angelos Panagiotopoulos <angelospanag@protonmail.com>
2024-05-27 17:29:01 +0300

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6,10 +6,9 @@ sudo apt install -y vim git jq python3-pip python3-venv pipx nmap pwgen
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # Go
-# https://go.dev/doc/install
-wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz && rm go1.22.2.linux-amd64.tar.gz
-echo "export PATH=\"\$PATH:/usr/local/go/bin\"" >> ~/.bashrc
+# https://launchpad.net/~longsleep/+archive/ubuntu/golang-backports
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update && sudo apt install golang-go
 
 # Node
 # https://nodejs.org/en/download/package-manager
