2857e93e620c5464b2fecbdf60d882af50ce4f94
Angelos Panagiotopoulos <angelospanag@protonmail.com>
2024-10-29 18:43:39 +0200

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,25 +1,29 @@
-# Install basic software
+# Upgrade current packages
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y vim git jq python3-pip python3-venv nmap pwgen
 
 # OhMyBash
 # https://github.com/ohmybash/oh-my-bash#basic-installation
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
-# Go
+# Go repo
 # https://launchpad.net/~longsleep/+archive/ubuntu/golang-backports
 sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt update && sudo apt install golang-go
 
-# Node
+# Python repo
+# https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+# Node repo
 # https://nodejs.org/en/download/package-manager
 # installs NVM (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 # download and install Node.js
-nvm install 20
+nvm install 22
 
-# Python
-# uv
+# Install packages
+sudo apt update && sudo apt install -y vim git jq python3.13-pip python3.13-venv nmap pwgen golang-go
+
+# Python - uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 # Ruff
 uv tool install ruff
